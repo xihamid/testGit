6c5444778496fdc1abd21eddcd1052aa96d832d9
xihamid <xihamid@gmail.com>
2023-04-11 14:26:24 +0500

    first commit

---
+++ key.js
@@ -0,0 +1,15 @@
+keys=sescretkeysaaaaa
+
+
+
+
+
+
+
+// git branch ye bataye ga kon c bracnch hy
+
+// git branch zain : zain k lie branch create krni hy
+// git checkout zain : branch say switchour krna hy 
+// git checkout -b ali : branch sy switch out b hoe ae hein or new b bana li hy
+// git checkout master: ab mjy master pr lay jao
+// git merge zain  : zain ki branch ko lao or merge kr do
